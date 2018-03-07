9b783a7d8353c95625ad58ca64c8a326c0a230f2
Grama Ioan-Emanuel <gramai.emanuel@gmail.com>
2018-03-07 09:48:23 +0200

    Update bittrex-markets-to-file.js

--- files/bittrex-markets-to-file.js
+++ files/bittrex-markets-to-file.js
@@ -14,7 +14,7 @@ class Trigger extends EventEmitter{
         var bitcoin_slice_ask=new Array(book_depth);
         var bitcoin_slice_bid=new Array (book_depth);
         //check if token_name is "ETH", in which case only data from "BTC-ETH" will be collected
-        if(market!="ETH"){ 
+        if(market!=="ETH"){ 
             marketManager.market('ETH-'+market, (err, ethereum) => {
                 ethereum.on('fills', console.log)
                 //fires each time changes have been applied to the orderbook, and prints the current state of the orderbook
@@ -24,9 +24,7 @@ class Trigger extends EventEmitter{
                     //i.e. [[ 0.10994992, 4.37637934 ], [ 0.10996992, 10.47637934 ] ...]
                     if(ethereum_slice_ask!=ethereum.asks.slice(0,book_depth) && ethereum_slice_bid!=ethereum.bids.slice(0,book_depth)){
                         ethereum_slice_ask = ethereum.asks.slice(0,book_depth);
-                        //console.log(ethereum_slice_ask);             
                         ethereum_slice_bid = ethereum.bids.slice(0,book_depth);
-                        //console.log(ethereum_slice_bid);
                         fs.writeFile(path+"/ETH-"+market+".txt", ethereum_slice_ask.concat(ethereum_slice_bid) , function(err) {
                             if(err) {
                                 return console.log(err);
@@ -47,9 +45,7 @@ class Trigger extends EventEmitter{
                     //i.e. [[ 0.10994992, 4.37637934 ], [ 0.10996992, 10.47637934 ] ...]
                 if(bitcoin_slice_ask!=bitcoin.asks.slice(0,book_depth) && bitcoin_slice_bid!=bitcoin.bids.slice(0,book_depth)){
                         bitcoin_slice_ask = bitcoin.asks.slice(0,book_depth);
-                        //console.log(bitcoin_slice_ask);
                         bitcoin_slice_bid = bitcoin.bids.slice(0,book_depth);
-                        //console.log(bitcoin_slice_bid);
                         fs.writeFile(path+"/BTC-"+market+".txt", bitcoin_slice_ask.concat(bitcoin_slice_bid) , function(err) {
                             if(err) {
                                 return console.log(err);
@@ -70,9 +66,7 @@ class Trigger extends EventEmitter{
                     //i.e. [[ 0.10994992, 4.37637934 ], [ 0.10996992, 10.47637934 ] ...]
                     if(bitcoin_slice_ask!=bitcoin.asks.slice(0,book_depth) && bitcoin_slice_bid!=bitcoin.bids.slice(0,book_depth)){
                         bitcoin_slice_ask = bitcoin.asks.slice(0,book_depth);
-                        //console.log(bitcoin_slice_ask);
                         bitcoin_slice_bid = bitcoin.bids.slice(0,book_depth);
-                        //console.log(bitcoin_slice_bid);
                         fs.writeFile(path+"/BTC-"+market+".txt", bitcoin_slice_ask.concat(bitcoin_slice_bid) , function(err) {
                             if(err) {
                                 return console.log(err);
